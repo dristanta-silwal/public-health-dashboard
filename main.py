ccdf9720c000fbdd051ca9279ce1be72d91a6504
Dristanta Silwal <dristantasilwal003@gmail.com>
2024-12-17 02:13:21 +0000

    dotenv added

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import os
 import psycopg2
+from dotenv import load_dotenv
 
-connection = psycopg2.connect(
-    dbname=os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT")
-)
+load_dotenv()
 
-cursor = connection.cursor()
-cursor.execute("SELECT version();")
-db_version = cursor.fetchone()
-print(f"Connected to: {db_version[0]}")
+dbname = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
 
-cursor.close()
-connection.close()
+try:
+    connection = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT version();")
+    db_version = cursor.fetchone()
+    print(f"Connected to: {db_version[0]}")
+
+    cursor.close()
+    connection.close()
+except psycopg2.Error as e:
+    print(f"Error connecting to the database: {e}")
